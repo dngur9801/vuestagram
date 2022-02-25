07e82c197930c564f662ea671bba46b36e742705
woohyuk jo <dngur98012@naver.com>
2022-02-25 12:51:05 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,139 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="template">
+    <div class="header">
+      <ul class="header-button-left">
+        <li>Cancel</li>
+      </ul>
+      <ul class="header-button-right">
+        <li>Next</li>
+      </ul>
+      <img src="./assets/logo.png" class="logo" />
+    </div>
+
+    <ContainerView :instaData="instaData" :step="step" />
+    <button @click="more">더보기</button>
+
+    <div class="footer">
+      <ul class="footer-button-plus">
+        <input type="file" id="file" class="inputfile" @change="upload" />
+        <label for="file" class="input-plus">+</label>
+      </ul>
+    </div>
+
+    <div v-if="step == 0">내용0</div>
+    <div v-if="step == 1">내용1</div>
+    <div v-if="step == 2">내용2</div>
+    <button @click="step = 0">버튼0</button>
+    <button @click="step = 1">버튼1</button>
+    <button @click="step = 2">버튼2</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import instaData from './assets/instadata.js';
+import ContainerView from './components/ContainerView.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      step: 0,
+      instaData: instaData,
+      moreCount: 0,
+    };
+  },
+  methods: {
+    more() {
+      axios
+        .get(
+          'https://codingapple1.github.io/vue/more' + this.moreCount + '.json'
+        )
+        .then(result => {
+          console.log(result.data);
+          this.instaData.push(result.data);
+          this.moreCount++;
+        });
+    },
+    upload(e) {
+      let file = e.target.files;
+      console.log(file[0].type);
+      let url = URL.createObjectURL(file[0]);
+      console.log(url);
+      this.step++;
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    ContainerView,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import 'style.css';
+body {
+  margin: 0;
+}
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
 }
 </style>
---
+++ src/components/PostView.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="template">
+    <div class="post" v-for="item in instaData" :key="item">
+      <div class="post-header">
+        <div
+          class="profile"
+          :style="{ backgroundImage: `url(${item.userImage})` }"
+        ></div>
+        <span class="profile-name">{{ item.name }}</span>
+      </div>
+      <div
+        class="post-body"
+        :style="{ backgroundImage: `url(${item.postImage})` }"
+      ></div>
+      <div class="post-content">
+        <p>{{ item.likes }}</p>
+        <p>
+          <strong>{{ item.filter }}</strong> {{ item.content }}
+        </p>
+        <p class="date">{{ item.date }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostView',
+  props: {
+    instaData: Array,
+  },
+};
+</script>
+
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+}
+</style>
